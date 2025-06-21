5f253da481e7a1861e49debf5ff7b6324df67e13
Alonso <llamucasebas@gmail.com>
2025-06-21 10:38:34 -0500

    fix key

--- src/sections/projects/data/data.tsx
+++ src/sections/projects/data/data.tsx
@@ -6,7 +6,7 @@ import CivaErpImage from "@/assets/civa-bg.png";
 
 export const data = [
   {
-    id: 3,
+    id: 1,
     title: "ERP Interno - Turismo CIVA",
     description:
       "Sistema ERP desarrollado para Turismo CIVA, una de las principales empresas de transporte terrestre en Perú. El sistema permite gestionar la venta de pasajes, envíos de encomiendas, sesiones de ventas, control de personal, administración de clientes y otros procesos internos clave.",
@@ -15,7 +15,7 @@ export const data = [
     isPrivate: true,
   },
   {
-    id: 1,
+    id: 2,
     title: "Sistema de asistencia",
     description:
       "Sistema de asistencia que realicé en la empresa Devdatep Consulting. El sistema permite llevar un control de asistencia de los empleados, generar reportes de asistencia y exportarlos a Excel. Además, incluye validación biométrica para el marcado de asistencia utilizando un servicios de AWS.",
@@ -33,7 +33,7 @@ export const data = [
     isPrivate: true,
   },
   {
-    id: 2,
+    id: 3,
     title: "Intranet",
     description:
       "En este proyecto realicé mantenimiento y mejoras en la intranet de la empresa Devdatep Consulting. La intranet es una aplicación web que permite a los empleados marcar su asistencia, ver sus reportes de asistencia, entre otras funcionalidades.",
@@ -50,7 +50,7 @@ export const data = [
     isPrivate: true,
   },
   {
-    id: 3,
+    id: 4,
     title: "Landing Page",
     description:
       "Landing page de la empresa Devdatep Consulting. La página web muestra información sobre los servicios que ofrece la empresa.",
